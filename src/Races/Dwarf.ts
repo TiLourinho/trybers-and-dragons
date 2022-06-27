60b43d87e70ca82650a2fa4c3e244d5e934228b6
TiLourinho <lourinho.tiago@gmail.com>
2022-06-27 16:24:39 -0300

    feat: Dwarf, Elf, Halfling and Orc classes

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,24 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this._createdRacesInstances += 1;
+
+    return this._createdRacesInstances;
+  }
+}
+
+export default Dwarf;
---
+++ src/Races/Orc.ts
@@ -0,0 +1,24 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 74;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this._createdRacesInstances += 1;
+
+    return this._createdRacesInstances;
+  }
+}
+
+export default Orc;
